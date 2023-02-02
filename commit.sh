6e0a8896cc04ca623220bf4594b158e39f304690
harish lalwani <harrie006@gmail.com>
2023-02-02 23:10:57 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,5 @@
 API_TOKEN_GITHUB=$AZUREPAT
-printenv AZUREPAT
-printenv secrets
 AZUSERNAME=$AZUSERNAME
-echo $AZUSERNAME
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$API_TOKEN_GITHUB@dev.azure.com/$AZUSERNAME/TestGitSync/_git/TestGitSync"
 git clone $GIT_CMD_REPOSITORY
 git add .
