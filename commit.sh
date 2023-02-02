18e3628d4aca435ad776f64a9b780757ff18471b
harish lalwani <harrie006@gmail.com>
2023-02-02 23:33:29 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,10 +4,12 @@ git clone https://github.com/harishlalwani/testAzureDevops
 cd testAzureDevops
 rm -rf .git
 
+cd ..
+
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$API_TOKEN_GITHUB@dev.azure.com/$AZUSERNAME/TestGitSync/_git/TestGitSync"
 git clone $GIT_CMD_REPOSITORY
 
-cp -r ../testAzureDevops/* TestGitSync/
+cp -r testAzureDevops/* TestGitSync/
 
 cd TestGitSync
 git add .
