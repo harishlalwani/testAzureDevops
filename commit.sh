e99827c6c8786e626e343d75c06a0d4a439478b9
harish lalwani <harrie006@gmail.com>
2023-02-02 23:41:29 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,6 @@
 API_TOKEN_GITHUB=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
+AZUSER_EMAIL=$AZUSER_EMAIL
 git clone https://github.com/harishlalwani/testAzureDevops
 cd testAzureDevops
 rm -rf .git
@@ -12,6 +13,10 @@ git clone $GIT_CMD_REPOSITORY
 cp -r testAzureDevops/* TestGitSync/
 
 cd TestGitSync
+
+git config --global user.email "$AZUSER_EMAIL"
+git config --global user.name "$AZUSERNAME"
+
 git add .
 git commit -m "sync from git to azure"
 ls -lrth
