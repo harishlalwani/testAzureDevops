33e02fc505f15f4b8dcc465885ee0eb56a5b4594
harish lalwani <harrie006@gmail.com>
2023-02-02 23:27:27 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,9 +1,12 @@
 API_TOKEN_GITHUB=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 git clone https://github.com/harishlalwani/testAzureDevops
+cd testAzureDevops
 rm -rf .git
 
-#GIT_CMD_REPOSITORY="https://$AZUSERNAME:$API_TOKEN_GITHUB@dev.azure.com/$AZUSERNAME/TestGitSync/_git/TestGitSync"
-#git clone $GIT_CMD_REPOSITORY
-#git add .
-#git push
+GIT_CMD_REPOSITORY="https://$AZUSERNAME:$API_TOKEN_GITHUB@dev.azure.com/$AZUSERNAME/TestGitSync/_git/TestGitSync"
+git clone $GIT_CMD_REPOSITORY
+
+mv -r ../testAzureDevops/* TestGitSync/
+git add .
+git push
